85e95874f3328ee3c8a638313eb01e5ec15758e6
khaledk8 <khalidkari1@gmail.com>
2023-09-05 09:57:34 +0300

    Feat: playround function

--- script.js
+++ script.js
@@ -22,6 +22,21 @@ function getPlayerSelection () {
 }
 
 
+function playRound (player, computer) {
+    if (player == computer) {
+        return "Draw!"
+    } else if ((player == "rock") && (computer == "paper")) {
+        return "You Lose! Paper beats Rock"
+    } else if ((player == "paper") && (computer == "rock")) {
+        return "You Win! Paper beats Rock"
+    } else if ((player == "rock") && (computer == "scissors")) {
+        return "You Win! Rock beats Scissors"
+    } else if ((player == "scissors") && (computer == "rock")) {
+        return "You Lose! Rock beats Scissors"
+    } else if ((player == "scissors") && (computer == "paper")) {
+        return "You Win! Scissors beats paper"
+    } else if ((player == "paper") && (computer == "scissors")) {
+        return "You Lose! Scissors beats paper"
+    }
+}
 
-console.log(getComputerChoice())
-getPlayerSelection()
