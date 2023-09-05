aff33908fbd285e8ed5989242bc99fdda9faca9a
khaledk8 <khalidkari1@gmail.com>
2023-09-05 10:19:33 +0300

    Added the game function and finished for now

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let p = 0
+let c = 0
 
 
 function getComputerChoice () {
@@ -24,19 +25,50 @@ function getPlayerSelection () {
 
 function playRound (player, computer) {
     if (player == computer) {
+        console.log("Draw!")
         return "Draw!"
     } else if ((player == "rock") && (computer == "paper")) {
+        c++
+        console.log("You Lose! Paper beats Rock")
         return "You Lose! Paper beats Rock"
     } else if ((player == "paper") && (computer == "rock")) {
+        p++
+        console.log("You Win! Paper beats Rock")
         return "You Win! Paper beats Rock"
     } else if ((player == "rock") && (computer == "scissors")) {
+        p++
+        console.log("You Win! Rock beats Scissors")
         return "You Win! Rock beats Scissors"
     } else if ((player == "scissors") && (computer == "rock")) {
+        c++
+        console.log("You Lose! Rock beats Scissors")
         return "You Lose! Rock beats Scissors"
     } else if ((player == "scissors") && (computer == "paper")) {
+        p++
+        console.log("You Win! Scissors beats paper")
         return "You Win! Scissors beats paper"
     } else if ((player == "paper") && (computer == "scissors")) {
+        c++
+        console.log("You Lose! Scissors beats paper")
         return "You Lose! Scissors beats paper"
     }
 }
 
+function game () {
+    playRound(getPlayerSelection(), getComputerChoice())
+    playRound(getPlayerSelection(), getComputerChoice())
+    playRound(getPlayerSelection(), getComputerChoice())
+    playRound(getPlayerSelection(), getComputerChoice())
+    playRound(getPlayerSelection(), getComputerChoice())
+    if (p > c) {
+        console.log("You Won!")
+    } else if (c > p) {
+        console.log("You Lost!")
+    } else if (c == p) {
+        console.log("You Drew with the Computer!")
+    }
+
+
+}
+
+game()
